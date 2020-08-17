d69d77434f042405fac3ed7e9a69af027b211161
Kelsey Myers <kelseymyerss@gmail.com>
2020-08-17 19:56:46 +0100

    Updates

---
+++ precache-manifest.ed98fff07c54a62793b14ec15bf5fda7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a373eccaa245c22db7",
+    "url": "/myrepo/static/css/main.74971ef4.chunk.css"
+  },
+  {
+    "revision": "f6a373eccaa245c22db7",
+    "url": "/myrepo/static/js/main.37cb8d85.chunk.js"
+  },
+  {
+    "revision": "c836ea5fcee3b1acce74",
+    "url": "/myrepo/static/js/runtime~main.e56f8662.js"
+  },
+  {
+    "revision": "412e7fa3114f263e54a9",
+    "url": "/myrepo/static/css/2.1420e50c.chunk.css"
+  },
+  {
+    "revision": "412e7fa3114f263e54a9",
+    "url": "/myrepo/static/js/2.99ef6828.chunk.js"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/myrepo/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "/myrepo/static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "da161ca3e4d5355283729b7862a55ad9",
+    "url": "/myrepo/static/media/KEL1.da161ca3.PNG"
+  },
+  {
+    "revision": "619d9c4cbe9e77ad72b34a0e682ef546",
+    "url": "/myrepo/static/media/KEL2.619d9c4c.PNG"
+  },
+  {
+    "revision": "9623861b480401fd28d98240b881133e",
+    "url": "/myrepo/static/media/KEL3.9623861b.PNG"
+  },
+  {
+    "revision": "0d1de66b4d44b42c9f682bda3cbae2cc",
+    "url": "/myrepo/static/media/KEL4.0d1de66b.PNG"
+  },
+  {
+    "revision": "144bb3349c813f709f576cebe47d1adf",
+    "url": "/myrepo/static/media/KEL5.144bb334.PNG"
+  },
+  {
+    "revision": "278e9b638ce17f436dff1a3666077dd3",
+    "url": "/myrepo/static/media/KEL6.278e9b63.PNG"
+  },
+  {
+    "revision": "287e3b768d526004d0109fed1dc49cba",
+    "url": "/myrepo/static/media/KEL7.287e3b76.PNG"
+  },
+  {
+    "revision": "ea8c0fac99bc6ac0a46d8a69101aecc1",
+    "url": "/myrepo/static/media/TT1.ea8c0fac.PNG"
+  },
+  {
+    "revision": "a8652c8bc52d071f5aba78b0aa1ebbc2",
+    "url": "/myrepo/static/media/TT2.a8652c8b.PNG"
+  },
+  {
+    "revision": "40a85b9e480346e59afba89d0a89e8b1",
+    "url": "/myrepo/static/media/TT3.40a85b9e.PNG"
+  },
+  {
+    "revision": "2174504c7c8145bd1fdc59f36b8aa6ab",
+    "url": "/myrepo/static/media/TT4.2174504c.PNG"
+  },
+  {
+    "revision": "276a2694b6aece752fe0c161c852dc36",
+    "url": "/myrepo/static/media/TT5.276a2694.PNG"
+  },
+  {
+    "revision": "7cad9ca82d879989808fbaf2b3af732b",
+    "url": "/myrepo/static/media/TT6.7cad9ca8.PNG"
+  },
+  {
+    "revision": "e4b554da62ec205ade2afaa4171e4295",
+    "url": "/myrepo/static/media/TT7.e4b554da.PNG"
+  },
+  {
+    "revision": "841f1a85c96992e02ff17711cade1ffa",
+    "url": "/myrepo/static/media/TT8.841f1a85.jpg"
+  },
+  {
+    "revision": "8b51d64435f1984947709b86cd92a123",
+    "url": "/myrepo/static/media/RAH1.8b51d644.PNG"
+  },
+  {
+    "revision": "330026d2c0d50831c47bd7864bca59b2",
+    "url": "/myrepo/static/media/RAH2.330026d2.PNG"
+  },
+  {
+    "revision": "bc0abe29d358d1abb2bce1470df68a6c",
+    "url": "/myrepo/static/media/RAH3.bc0abe29.PNG"
+  },
+  {
+    "revision": "a39f8272d894f7fd28d7078cf435e212",
+    "url": "/myrepo/static/media/RAH4.a39f8272.PNG"
+  },
+  {
+    "revision": "7be33c6e51b342d88d01418fa3af6883",
+    "url": "/myrepo/static/media/RAH5.7be33c6e.PNG"
+  },
+  {
+    "revision": "3e38024d50db27c5d61f5feb63f11646",
+    "url": "/myrepo/static/media/RAH6.3e38024d.PNG"
+  },
+  {
+    "revision": "c7df4ff0c923d6c65845eb6c9571750f",
+    "url": "/myrepo/static/media/RAH7.c7df4ff0.PNG"
+  },
+  {
+    "revision": "50057c3688ebb396ca4ac56927b6cbce",
+    "url": "/myrepo/static/media/RAH8.50057c36.PNG"
+  },
+  {
+    "revision": "bc4352d1648c5da2956ef3c4c7f95db0",
+    "url": "/myrepo/static/media/tictactoe.bc4352d1.png"
+  },
+  {
+    "revision": "8baa14323a06389a3210556c9f13b1d3",
+    "url": "/myrepo/static/media/google.8baa1432.png"
+  },
+  {
+    "revision": "85f93f8c5d271815883e75492b4e8f16",
+    "url": "/myrepo/static/media/todo.85f93f8c.png"
+  },
+  {
+    "revision": "8cfc2acbfe6478a66a07922733137bc8",
+    "url": "/myrepo/static/media/kelsey.8cfc2acb.jpg"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/myrepo/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "975458eb4230f9cb62e83bbb3ef2b52f",
+    "url": "/myrepo/index.html"
+  }
+];
