bb17284d35370cd2eb8ef15706e2105c29d8a518
Kelsey Myers <kelseymyerss@gmail.com>
2020-10-08 18:02:20 +0100

    Update 100Days.js

--- src/components/ux/100Days.js
+++ src/components/ux/100Days.js
@@ -8,16 +8,51 @@ const img1 =
   "https://ik.imagekit.io/kelseymyers0/UX/100_Days/Day1Mockup_w9ikM98DKcX.png";
 const img2 =
   "https://ik.imagekit.io/kelseymyers0/UX/100_Days/Day2Mockup_UhkMlJcVC.png";
+const img3 =
+  "https://ik.imagekit.io/kelseymyers0/UX/100_Days/Day3Mockup_lrJ0Dv4XIY.png";
+const img4 =
+  "https://ik.imagekit.io/kelseymyers0/UX/100_Days/Day3MockupW_2oo17ioyvvX.png";
+const img5 =
+  "https://ik.imagekit.io/kelseymyers0/UX/100_Days/Day4Mockup_dkNM86dO0.png";
 
 export default function HundredDays() {
   return (
     <div id="100days">
       <Carousel indicators={false} interval={null}>
         <Carousel.Item>
-          <img className="gallery-img hundred" src={img1} />
+          <img
+            className="gallery-img hundred"
+            src={img1}
+            alt="Mockup of Signin form"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="gallery-img hundred" src={img2} />
+          <img
+            className="gallery-img hundred"
+            src={img2}
+            alt="Mockup of Card Payment Page"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="gallery-img hundred"
+            src={img3}
+            alt="Mockup of Landing Page - Mobile"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="gallery-img hundred w-100"
+            src={img4}
+            alt="Mockup of Landing Page - Web"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="gallery-img hundred"
+            src={img5}
+            alt="Mockup of Grade Calculator"
+          />
         </Carousel.Item>
       </Carousel>
     </div>
