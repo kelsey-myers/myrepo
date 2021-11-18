5ff94494b91dfb4d8fc6bb4bd172c90823f1331d
Kelsey Myers <52179263+kelsey-myers@users.noreply.github.com>
2021-11-18 14:28:56 +0000

    Refactoring design page

--- src/components/ux/UX.js
+++ src/components/ux/UX.js
@@ -7,14 +7,46 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Fade from "react-reveal";
 
-import Environment from "./Environment";
-import Patisserie from "./Patisserie";
-import Beats from "./Beats";
-import MyRepo from "./MyRepo";
-import TTTradeV2 from "../portfolio/TTTradeV2";
-import HundredDays from "./100Days";
+import { Carousel } from "react-bootstrap";
+import UXDocs from "./UX.json";
 
 export default function Portfolio() {
+  const getImages = (project) => {
+    if (project.imageDetails.length > 1) {
+      return (
+        <Carousel indicators={false} interval={null}>
+          {project.imageDetails.map((image, index) => {
+            return (
+              <Carousel.Item className="mb-2">
+                <img
+                  className={
+                    image.orientation === "landscape"
+                      ? "gallery-img-ux w-100"
+                      : "gallery-img-ux"
+                  }
+                  src={image.link}
+                  alt={project.name + " screenshot " + index}
+                />
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      );
+    } else {
+      return (
+        <img
+          src={project.imageDetails[0].link}
+          className={
+            project.imageDetails[0].orientation === "landscape"
+              ? "gallery-img-ux w-100"
+              : "gallery-img-portrait-ux"
+          }
+          alt={project.imageDetails[0].alt}
+        />
+      );
+    }
+  };
+
   return (
     <div>
       <div id="projects">
@@ -31,114 +63,31 @@ export default function Portfolio() {
       </div>
       <Container fluid className="align-items-center p-5 background-white ">
         <Fade bottom>
-          <Row className="project-div-1 mb-5 align-items-center" xs={1} sm={2}>
-            <Col
-              xs={{ order: "last" }}
-              sm={{ order: "first" }}
-              className="text-center"
-            >
-              <HundredDays />
-            </Col>
-            <Col>
-              <h4 className="bebas text-center"> 100 Days of Design </h4>
-              <br />
-              <p className="lato text-center port-desc ml-auto mr-auto mb-0">
-                I decided to develop my UX skills my taking part in the 100 Days
-                of Design Challenge. Click on each image to enlarge it in a new
-                tab.
-              </p>
-            </Col>
-          </Row>
-
-          <Row className="project-div-1 mb-5 align-items-center" xs={1} sm={2}>
-            <Col xs={{ order: "last" }} className="text-center">
-              <TTTradeV2 />
-            </Col>
-            <Col>
-              <h4 className="bebas text-center"> TT Trade Quotes </h4>
-              <br />
-              <p className="lato text-center port-desc ml-auto mr-auto mb-0">
-                A simple modern design for a quote-finding company, TT Trade
-                Quotes. You can view the website{" "}
-                <a
-                  href="https://tttradequotes.co.uk"
-                  target="_blank"
-                  rel="noopener noreferrer"
+          {UXDocs.map((project, index) => {
+            return (
+              <Row
+                className="project-div-1 mb-5 align-items-center"
+                xs={1}
+                sm={2}
+                key={index}
+              >
+                <Col
+                  xs={{ order: "last" }}
+                  sm={{ order: index % 2 === 0 ? "last" : "first" }}
+                  className="text-center"
                 >
-                  here
-                </a>
-                .
-              </p>
-            </Col>
-          </Row>
-
-          <Row className="project-div-1 mb-5 align-items-center" xs={1} sm={2}>
-            <Col
-              xs={{ order: "last" }}
-              sm={{ order: "first" }}
-              className="text-center"
-            >
-              <MyRepo />
-            </Col>
-            <Col>
-              <h4 className="bebas text-center"> MyRepo </h4>
-              <br />
-              <p className="lato text-center port-desc  ml-auto mr-auto">
-                Oh hey it's my own website! This is a portfolio design which can
-                easily be customised to anyone's personal taste.
-              </p>
-            </Col>
-          </Row>
-
-          <Row className="project-div-2 mb-5 align-items-center" xs={1} sm={2}>
-            <Col xs={{ order: "last" }} className="text-center">
-              <Patisserie />
-            </Col>
-            <Col>
-              <h4 className="bebas text-center"> Patisserie </h4>
-              <br />
-              <p className="lato text-center port-desc  ml-auto mr-auto">
-                A few artboards from a mockup designed for a bakery client. The
-                brief was to provide a light and airy theme to the website, with
-                strong inspiration from French patisseries.
-              </p>
-            </Col>
-          </Row>
-
-          <Row className="project-div-1 mb-5 align-items-center" xs={1} sm={2}>
-            <Col
-              xs={{ order: "last" }}
-              sm={{ order: "first" }}
-              className="text-center"
-            >
-              <Beats />
-            </Col>
-            <Col>
-              <h4 className="bebas text-center"> Beats </h4>
-              <br />
-              <p className="lato text-center port-desc  ml-auto mr-auto">
-                A few artboards from a minimal shop theme designed by myself.
-                This was made primarily for technology-oriented businesses. The
-                landing page will be an autoplay video which displays the main
-                product.
-              </p>
-            </Col>
-          </Row>
-
-          <Row className="project-div-2 mb-5 align-items-center" xs={1} sm={2}>
-            <Col xs={{ order: "last" }} className="text-center">
-              <Environment />
-            </Col>
-            <Col>
-              <h4 className="bebas text-center"> Nature </h4>
-              <br />
-              <p className="lato text-center port-desc  ml-auto mr-auto">
-                A sample from a simple theme designed for environmental
-                organisations. The inspiration here was nature, simplicity, and
-                shades of green!
-              </p>
-            </Col>
-          </Row>
+                  {getImages(project)}
+                </Col>
+                <Col>
+                  <h4 className="bebas text-center"> {project.name} </h4>
+                  <br />
+                  <p className="lato text-center port-desc ml-auto mr-auto mb-0">
+                    {project.description}
+                  </p>
+                </Col>
+              </Row>
+            );
+          })}
         </Fade>
       </Container>
     </div>
