c8e84de3b5734db5b9fc29d1390315e480cf4707
Kelsey Myers <52179263+kelsey-myers@users.noreply.github.com>
2021-11-18 09:24:53 +0000

    update video

--- src/components/projects/Taskade.js
+++ src/components/projects/Taskade.js
@@ -3,7 +3,8 @@ import "../../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 /* Importing Gallery Images */
-const video = "https://ik.imagekit.io/kelseymyers0/Projects/Taskade_dchjjyh03";
+const video =
+  "https://ik.imagekit.io/kelseymyers0/Projects/Simulator_Screen_Recording_-_iPhone_11_-_2021-11-17_at_17.49.35_JeHxI1uRS.mp4?updatedAt=1637227464176";
 
 export default function Taskade() {
   return (
